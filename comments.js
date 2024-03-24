98945643fddf6d7fa7603cf6a38bcf2c4bdaf176
Gniang <jing.art@gmail.com>
2024-03-24 13:47:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// create web server
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import axios from 'axios';
+
+const app = express();
+app.use(bodyParser.json());
+
+// サーバー起動
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
